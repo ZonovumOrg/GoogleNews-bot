33f5781a0c31e83628e7b6f19ab48e5455caf00b
Zonovum <info.zonovum@gmail.com>
2021-01-05 18:59:53 +0100

    Edit the index.py file

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 # Important import
 from GoogleNews import GoogleNews
 import time
-
+import pandas as pd
 # Default params
 LANG = "en"
 PERIOD = "1Y"
@@ -9,17 +9,24 @@ KEY_WORD = "bitcoin"
 open('news.txt', 'a')
 # Important fonction
 news_csv_list = []
+dates = []
+titles = []
+descs = []
+links = []
 # while True:
+print('Loading... ', end=" ")
 googlenews = GoogleNews()
 googlenews.set_lang(LANG)
 googlenews.set_period(PERIOD)
 googlenews.get_news(KEY_WORD)
 news = googlenews.results()
-print(len(news))
-    # for x in news:
-    #     news = x
-    #     news_csv_list.append(news['title']+";"+news['desc']+";"+news['link']+";"+news['img']+";"+news['date'])
-    # csv_convert = "\n".join(news_csv_list)
-    # open('news.txt', 'w').write('title;desc;link;img;datetime\n%s'% csv_convert)
-    # time.sleep(60)
-    
+print('Done.')
+for x in news:
+    dates.append(x['datetime'])
+    titles.append(x['title'])
+    descs.append(x['desc'])
+    links.append(x['link'])
+df = pd.DataFrame({'dates': dates, 'titles': titles, 'descs': descs, 'links': links})
+csv_file = df.to_csv(index=False)
+open('news.txt', 'w').write(csv_file)
+print('New news file added.')
